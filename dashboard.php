0294eaa4373e58410823998eb161d04ab002f13e
stephen.okoro <sokoro@st.vvu.edu.gh>
2017-03-16 15:53:42 -0700

    Add files via upload
    guys i made some changes to the dashboard and the index page. drop your comments after viewing

---
+++ dashboard.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dash Board</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- //Custom Theme files -->
+<!-- web font -->
+<link href="//fonts.googleapis.com/css?family=Oswald:400,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+
+<style type="text/css">
+	img{
+		float: right;
+		margin-right: 450px;
+	}
+	
+</style>
+</head>
+
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+  <a class="navbar-brand" > Dash Board</a>
+    <ul class="nav navbar-nav navbar-right">
+  <li role="presentation" class="active"><a href="dashboard.php">Home</a></li>
+  <li role="presentation"><a href="adminlogin.php">Admin Login</a></li>
+  <li role="presentation"><a href="index.php">Staff Login</a></li>
+</ul>
+  </div>
+</nav>
+ <img src="images/vvu.png">
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+ <h1 align="center">VALLEY LIFE WATER MANAGEMENT SYSTEM</h1>
+</body>
+</html>
+
+
+
+
+
+
+  
+
