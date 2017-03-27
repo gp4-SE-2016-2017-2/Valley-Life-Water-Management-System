7e7c2ed6a73ae430910f0ab576ad82c1862d7b31
stephen.okoro <sokoro@st.vvu.edu.gh>
2017-03-27 08:17:51 -0700

    Add files via upload

---
+++ admindashboard.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title> Admin Dash Board</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- //Custom Theme files -->
+<!-- web font -->
+<link href="//fonts.googleapis.com/css?family=Oswald:400,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+<script type="text/javascript" src="js/jQuery.js"></script>
+
+<style type="text/css">
+	img{
+		float: right;
+		margin-right: 450px;
+	}
+	
+</style>
+</head>
+<body>
+     <nav class="navbar navbar-default">
+  <div class="container-fluid">
+  <a class="navbar-brand" >Admin Dash Board</a>
+   <ul class="nav navbar-nav navbar-right">
+   <li role="presentation"><a href="dashboard.php">Home</a></li>
+    <li role="presentation"><a href="adminlogin.php">Back</a></li>
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Staff <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Add New Staff</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Edit Staff</a></li>
+            <li role="separator" class="divider"></li>
+
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">View Reports<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Production Report</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Sales Report</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Inventory</a></li>
+           
+          </ul>
+        </li>
+      </ul>
+  </nav>
+  
+
+ <img src="images/vvu.png">
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+ <h1 align="center">VALLEY LIFE WATER MANAGEMENT SYSTEM</h1>
+
+<script src= "js/jQuery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
+
+
